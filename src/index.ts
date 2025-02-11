a0d36605c6f54372bb216c50330406511d0d95c6
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-02-11 21:06:53 +0530

    new commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-const users = new Map()
+const users = new Map<string,UserType>()
 users.set("xyz@123" ,{age : 21 , name : "xyz"},
     )
     users.set("abc@123" ,  {age : 22 , name : "abc"},)
 
     const user = users.get("xyz@123")
     console.log(user);
+
+    type UserType = {
+        name : string,
+        age : number,
+        email? : string
+    }
